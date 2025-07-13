17242276168342f3150f0d1ebe00aa4bd9446fa8
Clement Lebon <clement@engineai.com>
2025-07-13 21:51:20 +0200

    remove prefix

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "fr"],
   defaultLocale: "en",
+  localePrefix: 'never',
   domains: [
     {
       domain: "clementlbn.com",
